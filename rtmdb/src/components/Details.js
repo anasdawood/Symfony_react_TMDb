1cb8df7862dc7f260f05c70a4af76044292389f9
Anas Dawood <anas.dawood1@gmail.com>
2018-07-19 18:58:05 +0300

    t

--- rtmdb/src/components/Details.js
+++ rtmdb/src/components/Details.js
@@ -7,21 +7,46 @@ class Details extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      details: []
+      details: [],
+      genre: []
     };
   };
 
   componentDidMount() {
+    this.populateGenres();
     let userData = this.getUserData();
     getMovieDetails(this.props.params.dashId
     ).then((data) => {
       this.setState({ details: data });
-      console.log(data);
+      console.log(this.state.genres);
     })
       .catch((err) => {
         console.error('err', err);
       });
+  }
 
+  populateGenres() {
+    this.state.genres = {
+      28: "Action",
+      12: "Adventure",
+      16: "Animation",
+      35: "Comedy",
+      80: "Crime",
+      99: "Documentary",
+      18: "Drama",
+      10751: "Family",
+      14: "Fantasy",
+      36: "History",
+      27: "Horror",
+      10402: "Music",
+      9648: "Mystery",
+      10749: "Romance",
+      878: "Science Fiction",
+      10770: "TV Movie",
+      53: "Thriller",
+      10752: "War",
+      37: "Western"
+    }
   }
 
   getUserData() {
